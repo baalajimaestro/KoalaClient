390e2af71ffe209464137c5b3c0bc064e07a1bdf
Yardanico <tiberiumk12@gmail.com>
2023-08-17 01:54:02 +0300

    Update helper.ts

--- src/api/helper.ts
+++ src/api/helper.ts
@@ -1,17 +1,14 @@
 import { EventSourceData } from "@type/api";
 
 export const parseEventSource = (
-  data: string,
-): "[DONE]" | EventSourceData[] => {
+  data: string
+): '[DONE]' | EventSourceData[] => {
   const result = data
-    .split("\n\n")
-    .filter(Boolean)
-    .map((chunk) => {
-      const jsonString = chunk
-        .split("\n")
-        .map((line) => line.replace(/^data: /, ""))
-        .join("");
-      if (jsonString === "[DONE]") return jsonString;
+        .split('\n')
+    .filter(line => line.startsWith('data: ') || line === '[DONE]')
+    .map((line) => {
+      if (line === '[DONE]') return line;
+      const jsonString = line.replace(/^data: /, '');
       try {
         const json = JSON.parse(jsonString);
         return json;
